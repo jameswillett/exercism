47ca4036b2adf98ebcba9dbfbb54d07d712bfe76
James Willett <james@Jamess-iMac.fios-router.home>
2018-11-16 01:44:04 -0500

    alt hs isogram

--- haskell/isogram/src/Isogram.hs
+++ haskell/isogram/src/Isogram.hs
@@ -1,7 +1,16 @@
 module Isogram (isIsogram) where
 
 import Data.Char (toLower, isLetter)
-import Data.List (sort, group)
+import Data.List (sort, group, nub)
+
+normalize :: String -> String
+normalize = map toLower . filter isLetter
 
 isIsogram :: String -> Bool
-isIsogram = not . any ((>1) . length) . group . sort . map toLower . filter isLetter
+isIsogram = not . any ((>1) . length) . group . sort . normalize
+
+isIsogram' :: String -> Bool
+isIsogram' w = lw == lw'
+  where n   = normalize w
+        lw  = length n
+        lw' = length $ nub n
