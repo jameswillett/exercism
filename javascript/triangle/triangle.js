66d2a2d11138e2a1bc4d088271283bca1c376390
James Willett <james@Jamess-iMac.fios-router.home>
2018-11-18 00:15:16 -0500

    cleaning up triangle js

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,43 +1,29 @@
 const R = require('ramda');
 
+const sortDecending = R.sort(R.flip(R.subtract));
+
 export default class Triangle {
   constructor(...sides) {
-    this.sides = sides;
+    this.sides = sortDecending(sides);
   }
 
   kind() {
+    const [max, ...legs] = this.sides;
+    const min = R.last(legs);
     const types = ['equilateral', 'isosceles', 'scalene'];
-    const byDecending = R.flip(R.subtract);
 
-    // if any legs less than or equal to 0 throw error
-    if (R.any(R.flip(R.lte)(0), this.sides)) {
+    if (min <= 0) {
       throw Error('zero and negative values not allowed');
     }
 
-    // sort sides in decending order then destructure head and tail
-    // [4, 5, 5] -> [5, 5, 4] -> max == 5, legs = [5,4]
-    const [max, ...legs] = R.sort(byDecending, this.sides);
-
-    // pretty self explanitory
     if (R.sum(legs) < max) {
       throw Error('those values cant make a triangle');
     }
 
     const getType = R.pipe(
-
-      // sort sides [3,5,3] -> [5,3,3]
-      R.sort(byDecending),
-
-      // group adjacent equal values ... -> [[5], [3,3]]
       R.groupWith(R.equals),
-
-      // get the length ... -> 2
       R.length,
-
-      // decrement ... -> 1
       R.dec,
-
-      // grab triangle type at that index ... -> "isosceles"
       R.flip(R.prop)(types),
     );
 
