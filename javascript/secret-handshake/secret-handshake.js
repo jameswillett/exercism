b2918a9ed76c1b9212143e949b81e09eadabd913
James Willett <james@Jamess-iMac.fios-router.home>
2018-11-19 03:08:20 -0500

    ALL RAMDA handshake js

--- javascript/secret-handshake/secret-handshake.js
+++ javascript/secret-handshake/secret-handshake.js
@@ -4,14 +4,17 @@ const actions = ['wink', 'double blink', 'close your eyes', 'jump'];
 
 const secretHandshake = (n) => {
   if (isNaN(n)) throw new Error('Handshake must be a number'); // eslint-disable-line
-  
-  const moves = R.compose(
-    R.map(([_, x]) => x), // eslint-disable-line
-    R.filter(([x]) => x),
-    R.zip(R.map(Number, R.reverse(n.toString(2)))),
-  )(actions);
 
-  return n >= 16 ? moves.reverse() : moves;
+  return R.compose(
+    R.unless(_ => n < 16, R.reverse), // eslint-disable-line
+    R.map(R.head),
+    R.filter(R.last),
+    R.zip(actions),
+    R.map(Number),
+    R.reverse,
+  )(n.toString(2));
+
+  // return n >= 16 ? moves.reverse() : moves;
 };
 
 module.exports = { secretHandshake };
