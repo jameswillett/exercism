d39fcfdd16d91fea0bee22e573dbe5796f4fb716
James Willett <james@Jamess-iMac.fios-router.home>
2018-11-18 23:10:31 -0500

    so terse

--- haskell/grade-school/src/School.hs
+++ haskell/grade-school/src/School.hs
@@ -7,18 +7,13 @@ import qualified Data.Map as M
 type School = Map Int [String]
 
 add :: Int -> String -> School -> School
-add gradeNum student school
-  | M.member gradeNum school  = M.adjust (L.sort . (:) student) gradeNum school
-  | otherwise                 = M.insert gradeNum [student] school
+add gradeNum student school = M.insertWith ((L.sort .) . (++)) gradeNum [student] school
 
 empty :: School
 empty = M.empty
 
 grade :: Int -> School -> [String]
-grade gradeNum school = 
-  case (M.lookup gradeNum school) of
-    Just students -> students
-    Nothing       -> []
+grade = M.findWithDefault []
 
 sorted :: School -> [(Int, [String])]
 sorted = M.toAscList
