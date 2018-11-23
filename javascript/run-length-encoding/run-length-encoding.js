401ba1cab8f01bd4419db9b1bc20749a990f3b88
James Willett <james@Jamess-iMac.fios-router.home>
2018-11-23 11:09:25 -0500

    rle js

---
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -0,0 +1,25 @@
+const R = require('ramda');
+
+const isNumber = R.flip(R.includes)(R.map(R.toString, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+const flattenToCounts = (a, c) => {
+  if (c.length === 1) return a + R.head(c);
+  return a + c.length + R.head(c);
+};
+
+const expandFromCounts = (a, c) => {
+  if (c.length === 1) return a + c;
+  return a + R.join('', R.repeat(R.last(c), R.init(c)));
+};
+
+const encode = R.pipe(
+  R.groupWith(R.equals),
+  R.reduce(flattenToCounts, ''),
+);
+
+const decode = R.pipe(
+  R.groupWith(isNumber), // eslint-disable-line
+  R.reduce(expandFromCounts, ''),
+);
+
+module.exports = { encode, decode };
