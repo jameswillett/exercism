213b56ecd3fe7350f1627d97737298d9a8153b57
James Willett <jms.wlltt@gmail.com>
2019-01-18 15:38:26 -0500

    one liner hamming

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,9 +1,6 @@
 const compute = (left, right) => {
   if (left.length !== right.length) throw new Error('left and right strands must be of equal length');
-  return [...left].reduce((count, l, i) => {
-    if (l !== right[i]) return count + 1;
-    return count;
-  }, 0);
+  return [...left].reduce((c, l, i) => c + (l !== right[i]), 0);
 };
 
 module.exports = { compute };
